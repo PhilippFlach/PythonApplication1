dc8421dbfb939cef0ca358c71fb65acdc4588d21
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2020-04-16 11:30:34 +0000

    13.30

--- PythonApplication1/session2.py
+++ PythonApplication1/session2.py
@@ -1,32 +1,42 @@
+###################
 # name = 'Philipp'
-
 # print(name)
 # print("Type: " + str(type(name)))
 
 # name = iter(name)
 # print("Type: " + str(type(name)))
-
 # # next()
 
 # cursor = next(name)
 # print(cursor)
-
-
 # cursor = next(name)
 # print(cursor)
+###################
 
 
-d1 = {'header1': 'value1', 'header2': 'value2'}
+
+###################
+# d1 = {'header1': 'value1', 'header2': 'value2'}
 
 
-for key, value in d1.items(): # ('header1', 'value1') # the tuple unpacked automatically
-    print(key, value)
+# for key, value in d1.items(): # ('header1', 'value1') # the tuple unpacked automatically
+#     print(key, value)
 
 
-for count, t1 in enumerate(d1.items()): # ('header1', 'value1') # enumerate() # Enumerations made the unpack unable
-    print(count, t1[0], t1[1])
+# for count, t1 in enumerate(d1.items()): # ('header1', 'value1') # enumerate() # Enumerations made the unpack unable
+#     print(count, t1[0], t1[1])
+###################
 
 
+###################
+# reading files
+our_file = open('testdata/linearregression.csv', 'r')
+# print('Type: ' + str(type(our_file)))
+
+for row in our_file.readlines():
+    print(row, end='')
+
+our_file.close()
 
 
 
