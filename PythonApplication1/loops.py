bc725cf64703be58f0787e2559fe362f5a6949b7
Philipp Flach <flach.philipp@luzi.ch>
2020-04-16 11:53:54 +0200

    Preparation for next training session

---
+++ PythonApplication1/loops.py
@@ -0,0 +1,7 @@
+student_names = ["Allan", "Katarina", "Mark", "Jessica", "Mr. Bean", 
+                 "Sarah"]
+
+for name in student_names:
+    if name == "Mr. Bean":
+        print("Found him! " + name)
+    print("Currently testing " + name)
